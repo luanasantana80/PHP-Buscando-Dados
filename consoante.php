21831caca81dc1eed00d87f438a83f6044bf38ba
Luana Caroline <93920703+luanasantana80@users.noreply.github.com>
2023-01-11 14:07:05 -0300

    Add files via upload

---
+++ consoante.php
@@ -0,0 +1,22 @@
+<?php
+
+  switch ($cons) {
+    case 'a': echo "Vogal"
+      break;
+
+    case 'e': echo "Vogal"
+      break;
+
+    case 'i': echo "Vogal"
+      break;
+    
+    case 'o': echo "Vogal"
+      break;
+
+    case 'u': echo "Vogal"
+      break;
+    default:
+      echo "consoante"
+      break;
+  }
+?>  
